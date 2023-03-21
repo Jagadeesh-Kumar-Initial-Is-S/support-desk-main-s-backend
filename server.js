07290ef4b7f89f7aad7951f0281952e9eb4cdc54
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-03-21 17:14:00 +0530

    Commit

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ if (process.env.NODE_ENV === 'production') {
   })
 } else {
   app.get('/', (_, res) => {
-    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+    res.status(200).json({ message: 'Welcome to the Support Desk API',
+    advertisement: 'Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me, you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@ybl .' })
   })
 }
 
