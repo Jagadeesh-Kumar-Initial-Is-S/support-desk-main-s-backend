4513c87d67a3aa3a56ace20b2e7201a2f22c30c0
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-03-22 19:49:14 +0530

    Commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const express = require('express')
-var cors = require('cors')
+const cors = require('cors')
 require('colors')
 require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
@@ -12,7 +12,12 @@ connectDB()
 
 const app = express()
  
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}));
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
